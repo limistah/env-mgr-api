12dc4a4c02b0ed4e18dd29456ec8add0acc46af0
Aleem Isiaka <aleemisiaka@gmail.com>
2021-09-12 17:07:54 +0100

    Postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,8 @@ console.log(process.env);
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DATABASE,
       synchronize: true,
